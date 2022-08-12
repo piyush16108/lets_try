8ea364c411e6dac0780d11d93c9c9f343af85670
piyush16108 <100013910+piyush16108@users.noreply.github.com>
2022-08-12 19:41:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var font_size = 10;
 $(document).ready(function(){
-			alert("Position: sticky");
+			alert("Position: sticky bottom");
 			$("#nav-placeholder").load("https://piyush16108.github.io/lets_try/nav.html");
 			
 			var handle = $( "#custom-handle" );
