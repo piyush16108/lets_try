6ea1521f7f41a154ee025694d8fbdfd7e71264ff
piyush16108 <100013910+piyush16108@users.noreply.github.com>
2022-08-12 19:54:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var font_size = 10;
 $(document).ready(function(){
-			alert("Position: sticky bottom");
+			alert("No slider");
 			$("#nav-placeholder").load("https://piyush16108.github.io/lets_try/nav.html");
 			
 			var handle = $( "#custom-handle" );
